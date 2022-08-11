dbcf4b81062705e30ecf8ed7a5a2782a91c90fb9
Lígia Bicalho <ligiabicalho@gmail.com>
2022-08-11 15:51:05 -0300

    Requisito 3

---
+++ script.js
@@ -0,0 +1,13 @@
+const btnLogin = document.querySelector('#btn-login');
+const email = document.querySelector('#email');
+const senha = document.querySelector('#password');
+
+function login() {
+  if (email.value === 'tryber@teste.com' && senha.value === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
+}
+
+btnLogin.addEventListener('click', login);
