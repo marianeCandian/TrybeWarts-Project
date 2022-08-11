3a8c5fa6491734047efa200dbe4bc8152fd17cae
ThiagoShundi <shundi_hayashi@hotmail.com>
2022-08-11 16:42:09 -0300

    Requisitos 10 e 11

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-const btnLogin = document.querySelector('#btn-login');
-const email = document.querySelector('#email');
-const senha = document.querySelector('#password');
+const btnLogin = document.querySelector("#btn-login");
+const email = document.querySelector("#email");
+const senha = document.querySelector("#password");
 
 function login() {
-  if (email.value === 'tryber@teste.com' && senha.value === '123456') {
-    alert('Olá, Tryber!');
+  if (email.value === "tryber@teste.com" && senha.value === "123456") {
+    alert("Olá, Tryber!");
   } else {
-    alert('Email ou senha inválidos.');
+    alert("Email ou senha inválidos.");
   }
 }
 
-btnLogin.addEventListener('click', login);
+btnLogin.addEventListener("click", login);
