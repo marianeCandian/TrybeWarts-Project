5a13ab8a0d79f3a23f5fc9b1bf38451e5c3bd2d3
Mariane Candian <mariane.candian@gmail.com>
2022-08-11 17:53:38 -0300

    Requisito 18

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
-const btnLogin = document.querySelector("#btn-login");
-const email = document.querySelector("#email");
-const senha = document.querySelector("#password");
+const btnLogin = document.querySelector('#btn-login');
+const email = document.querySelector('#email');
+const senha = document.querySelector('#password');
 
 function login() {
-  if (email.value === "tryber@teste.com" && senha.value === "123456") {
-    alert("Olá, Tryber!");
+  if (email.value === 'tryber@teste.com' && senha.value === '123456') {
+    alert('Olá, Tryber!');
   } else {
-    alert("Email ou senha inválidos.");
+    alert('Email ou senha inválidos.');
   }
 }
 
-btnLogin.addEventListener("click", login);
+btnLogin.addEventListener('click', login);
+
+const bntEnviar = document.querySelector('#submit-btn');
+const checkBox = document.querySelector('#agreement');
+
+function habilitar() {
+  bntEnviar.disabled = !checkBox.checked;
+}
+
+checkBox.addEventListener('change', habilitar);
