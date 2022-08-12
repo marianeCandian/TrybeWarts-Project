c6a60b42bf0f16a69aa3186d9a0984f6cd6de77b
Lígia Bicalho <ligiabicalho@gmail.com>
2022-08-12 00:29:39 -0300

    Requisito 21 - em andamento

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const btnLogin = document.querySelector('#btn-login');
-const email = document.querySelector('#email');
+const emailLogin = document.querySelector('#email');
 const senha = document.querySelector('#password');
 
 function login() {
-  if (email.value === 'tryber@teste.com' && senha.value === '123456') {
+  if (emailLogin.value === 'tryber@teste.com' && senha.value === '123456') {
     alert('Olá, Tryber!');
   } else {
     alert('Email ou senha inválidos.');
@@ -30,3 +30,65 @@ function limitTextArea(valor) {
   }
 }
 
+const nome = document.querySelector('#input-name');
+const sobrenome = document.querySelector('#input-lastname');
+const email = document.querySelector('#input-email');
+const casa = document.querySelector('#house');
+const familia = document.querySelector('#label-family');
+const conteudo = document.querySelector('#label-content');
+const avaliacao = document.querySelector('#label-rate');
+const observacoes = document.querySelector('#textarea');
+const forms = document.querySelector('#form-data');
+
+function getCasa() {
+  return casa.options[casa.selectedIndex].value;
+}
+
+function getFamilia () {
+  for (let i = 0; i < familia.length; i += 1) {
+    if (familia[i].checked) {
+      return familia[i].value;
+    }
+  }
+}
+
+function getConteudo () {
+  for (let i = 0; i < conteudo.length; i += 1) {
+    if (conteudo[i].checked) {
+      return conteudo[i].value;
+    }
+  }
+}
+
+function getAvaliacao() {
+  return avaliacao.options[avaliacao.selectedIndex].value;
+}
+
+const pNomeSobrenome = document.createElement('p');
+const pEmail = document.createElement('p');
+const pCasa = document.createElement('p');
+const pFamilia = document.createElement('p');
+const pConteudo = document.createElement('p');
+const pAvaliacao = document.createElement('p');
+const pObservacoes = document.createElement('p');
+
+function addItemForms (e) {
+  e.preventDefault();
+  pNomeSobrenome.innerText = 'Nome: ' + nome.value + sobrenome.value;
+  pEmail.innerText = 'Email: ' + email.value;
+  pCasa.innerText = 'Casa :' + getCasa();
+  pFamilia.innerText = 'Familia: ' + getFamilia();
+  pConteudo.innerText = 'Matérias: ' + getConteudo();
+  pAvaliacao.innerText = 'Avaliação: ' + getAvaliacao();
+  pObservacoes.innerText = 'Observações: ' + observacoes.value;
+  forms.appendChild(pNomeSobrenome);
+  forms.appendChild(pEmail);
+  forms.appendChild(pCasa);
+  forms.appendChild(pFamilia);
+  forms.appendChild(pConteudo);
+  forms.appendChild(pAvaliacao);
+  forms.appendChild(pObservacoes);
+  console.log('Oi');
+}
+
+bntEnviar.addEventListener('click', addItemForms);
